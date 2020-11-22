311f3e36b8bbfa09d3f862ce9a1260c75529d96f
Fatimah Alhelal <31361308+fatmahhelal@users.noreply.github.com>
2020-11-22 23:26:54 +0300

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   auth0: {
     url: 'footy.us', // the auth0 domain prefix
     audience: 'coffee', // the audience set for the auth0 app
-    clientId: '9tFarTJLGVj7vZLMv5jEfq9voWIMSk6x', // the client id generated for the auth0 app
+    clientId: '', // the client id generated for the auth0 app
     callbackURL: 'http://127.0.0.1:8100', // the base url of the running ionic application. 
   }
 };
